70b863e0cafbe20f1d2df4e5ca18cda90891a50b
Трегубов Сергей <booba@yandex.ru>
2025-12-17 10:12:30 +0800

    js карусели

--- js/projects-current-carousel.js
+++ js/projects-current-carousel.js
@@ -14,6 +14,68 @@ export default class ProjectsCurrentCarousel {
     
     init() {
         this.swiper = new Swiper(this.container, {
+
+        // Количество слайдов в зависимости от ширины экрана
+        slidesPerView: 1,
+        spaceBetween: 20,
+        
+        // Адаптивные настройки
+        breakpoints: {
+            // На телефонах
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 15
+            },
+            // На планшетах
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // На ноутбуках
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 25
+            },
+            // На больших экранах
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        },
+        
+        // Навигация
+        navigation: {
+            nextEl: '.screenshots-slider .swiper-button-next',
+            prevEl: '.screenshots-slider .swiper-button-prev',
+        },
+        
+        // Пагинация
+        pagination: {
+            el: '.screenshots-slider .swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+        },
+        
+        // Дополнительные настройки
+        loop: false,
+        grabCursor: true,
+        speed: 500,
+        
+        // Эффекты
+        effect: 'slide',
+        
+        // Автовысота
+        autoHeight: false,
+        
+        // Центрирование активного слайда
+        centeredSlides: false,
+        
+        // Отступы от краев
+        slidesOffsetBefore: 0,
+        slidesOffsetAfter: 0
+
+
+/*
             slidesPerView: 1,
             spaceBetween: 20,
             navigation: {
@@ -28,6 +90,9 @@ export default class ProjectsCurrentCarousel {
                 768: { slidesPerView: 1.5, centeredSlides: true },
                 1024: { slidesPerView: 2 }
             }
+*/
+
+
         });
     }
 }
